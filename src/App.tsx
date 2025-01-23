0e704bc81e00395e92f083326715936e190dc719
Bernardo Rodrigues <bfar97@gmail.com>
2025-01-23 12:11:14 +0000

    second

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App(): JSX.Element {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to React + Bun + TypeScrip</h1>
+      <h1>Welcome to React + Bun + TypeScript</h1>
       <p>Start editing src/App.tsx to make changes</p>
     </div>
   );
